660da49b1489e6e728e0ff0f5f13968bea6b6321
Eric Stansifer <eric.stansifer+github@gmail.com>
2025-09-06 19:41:07 -0400

    dkfjn

--- src/query.py
+++ src/query.py
@@ -3,9 +3,25 @@ import random
 import interface
 import tasks
 
-doors = [0, 1, 2, 3, 4, 5]
-
 r = random.choices
+
+class Door:
+    def __init__(self, d):
+        self.d = d
+        self.is_door = True
+
+    def __str__(self):
+        return str(self.d)
+
+doors = [Door(d) for d in range(6)]
+
+class Mark:
+    def __init__(self, m):
+        self.m = m
+        self.is_door = False
+
+    def __str__(self):
+        return f'[{self.m}]'
 
 class Query:
     def __init__(self, task = None):
@@ -14,30 +30,51 @@ class Query:
         self.task = task
         self.N = task.N
         self.query_length = task.query_length
+
         self.query = None
+        self.query_doors_only = None
         self.query_string = None
         self.response = None
 
-        self.make_query()
+        self.random_query1()
 
-    def make_query(self):
+    def custom_query(self, actions, trim_if_too_long = True, error_if_too_short = True):
+        self.query = list(actions)
+        if trim_if_too_long:
+            self.query = self.query[:self.query_length]
+        if error_if_too_short:
+            assert len(self.query) == self.query_length
+
+        self.query_doors_only = [q for q in self.query if q.is_door]
+
+        self.query_string = ''.join([str(act) for act in self.query])
+
+    def random_query1(self):
         q = []
         while len(q) < self.query_length:
             d = r(doors, [5, 5, 2, 2, 2, 2])[0]
             q.append(d)
 
-        self.query = q
-        self.query_string = ''.join([str(d) for d in q])
-        assert len(self.query_string) == self.query_length
+        self.custom_query(q)
+
+    def parse_response(self, response):
+        self.raw_response = response
+        assert len(self.query) + 1 == len(self.raw_response)
+
+        # Only keep responses to movements, not markings
+        self.response = [response[0]]
+        for i in range(len(self.query)):
+            if self.query[i].is_door:
+                self.response.append(response[i + 1])
+
+        assert len(self.query_doors_only) + 1 == len(self.response)
 
     def submit(self):
-        assert len(self.query_string) == self.query_length
-
         j = interface.explore([self.query_string])
         results = j['results'][0]
         query_count = j['queryCount']
 
-        self.response = results
+        self.parse_response(results)
 
         return query_count
 
@@ -45,19 +82,16 @@ def submit_batch(queries):
     if len(queries) == 0:
         return
 
-    N = queries[0].N
-    for q in queries:
-        assert len(q.query_string) == q.query_length
-        assert q.N == N
-
     j = interface.explore([q.query_string for q in queries])
     results = j['results']
     query_count = j['queryCount']
     for i, q in enumerate(queries):
-        q.response = results[i]
+        q.parse_response(results[i])
 
     return query_count
 
 if __name__ == '__main__':
     q = Query()
+    print(q.query_string)
+    q.custom_query([Door(0), Mark(3), Door(1)], error_if_too_short = False)
     print(q.query_string)
